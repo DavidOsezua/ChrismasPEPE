50aea7d41e9c9271d041b9135bd4abf08f1e8d1d
Ttech5 <ttech7633@gmail.com>
2023-12-05 19:13:44 +0100

    Connect contract

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
-import styles from './Navbar.module.css';
-import { logo, linkArrow } from '../assets';
+import { useState } from "react";
+import styles from "./Navbar.module.css";
+import { logo, linkArrow } from "../assets";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { useAccount } from "wagmi";
 
 const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
+
+  const { open } = useWeb3Modal();
+  const { address } = useAccount();
 
   const changeNavbarColor = () => {
     if (window.scrollY >= 55) {
@@ -13,7 +18,11 @@ const Navbar = () => {
     }
   };
 
-  window.addEventListener('scroll', changeNavbarColor);
+  const shortenAddress = (address) => {
+    return `${address.slice(0, 6)}...${address.slice(-4)}`;
+  };
+
+  window.addEventListener("scroll", changeNavbarColor);
   return (
     <header
       className={`${
@@ -21,21 +30,22 @@ const Navbar = () => {
       }`}
     >
       <nav className={`${styles.navContainer}`}>
-        <img src={logo} alt='Logo' className={styles.logo} />
-        <div className='flex items-center gap-[3px]'>
+        <img src={logo} alt="Logo" className={styles.logo} />
+        <div className="flex items-center gap-[3px]">
           <a
-            href='http://christmaspepe.com/'
-            className='text-[#fff] text-[300]'
+            href="http://christmaspepe.com/"
+            className="text-[#fff] text-[300]"
           >
-            {' '}
-            Back Home{' '}
+            {" "}
+            Back Home{" "}
           </a>
           <img src={linkArrow} />
         </div>
         <button
-          className={` bg-white rounded-[3px] [background:linear-gradient(180deg,rgb(57,169,74)_0%,rgb(116.52,246.93,137.39)_100%)] text-[#fff] ${styles.btn}`}
+          className={` px-2 bg-white rounded-[3px] [background:linear-gradient(180deg,rgb(57,169,74)_0%,rgb(116.52,246.93,137.39)_100%)] text-[#fff] ${styles.btn}`}
+          onClick={open}
         >
-          Connect
+          {address ? shortenAddress(address) : "Connect Wallet"}
         </button>
       </nav>
     </header>
